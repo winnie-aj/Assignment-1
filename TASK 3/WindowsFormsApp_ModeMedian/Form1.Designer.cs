830322ecd36f0fa6b708b2ff449b3a4223c44542
winnie-aj <75024329+winnie-aj@users.noreply.github.com>
2021-02-08 23:24:12 +0000

    Add files via upload

---
+++ TASK 3/WindowsFormsApp_ModeMedian/Form1.Designer.cs
@@ -0,0 +1,274 @@
+﻿
+namespace WindowsFormsApp_ModeMedian
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtMean = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.txtValue = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtMode = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtMedian = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtStandardDev = new System.Windows.Forms.TextBox();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.lblCount = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtVariance = new System.Windows.Forms.TextBox();
+            this.lbxValues = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.label1.Location = new System.Drawing.Point(47, 48);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Value";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtMean
+            // 
+            this.txtMean.Location = new System.Drawing.Point(139, 282);
+            this.txtMean.Margin = new System.Windows.Forms.Padding(4);
+            this.txtMean.Name = "txtMean";
+            this.txtMean.Size = new System.Drawing.Size(283, 22);
+            this.txtMean.TabIndex = 1;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(323, 42);
+            this.btnAdd.Margin = new System.Windows.Forms.Padding(4);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(100, 28);
+            this.btnAdd.TabIndex = 2;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(47, 290);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 17);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Mean";
+            // 
+            // btnClear
+            // 
+            this.btnClear.ForeColor = System.Drawing.Color.Red;
+            this.btnClear.Location = new System.Drawing.Point(323, 95);
+            this.btnClear.Margin = new System.Windows.Forms.Padding(4);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(100, 28);
+            this.btnClear.TabIndex = 2;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // txtValue
+            // 
+            this.txtValue.Location = new System.Drawing.Point(139, 44);
+            this.txtValue.Margin = new System.Windows.Forms.Padding(4);
+            this.txtValue.Name = "txtValue";
+            this.txtValue.Size = new System.Drawing.Size(132, 22);
+            this.txtValue.TabIndex = 1;
+            this.txtValue.Text = "55";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(47, 322);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 17);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Mode";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // txtMode
+            // 
+            this.txtMode.Location = new System.Drawing.Point(139, 314);
+            this.txtMode.Margin = new System.Windows.Forms.Padding(4);
+            this.txtMode.Name = "txtMode";
+            this.txtMode.Size = new System.Drawing.Size(283, 22);
+            this.txtMode.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(47, 354);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 17);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Median";
+            // 
+            // txtMedian
+            // 
+            this.txtMedian.Location = new System.Drawing.Point(139, 346);
+            this.txtMedian.Margin = new System.Windows.Forms.Padding(4);
+            this.txtMedian.Name = "txtMedian";
+            this.txtMedian.Size = new System.Drawing.Size(283, 22);
+            this.txtMedian.TabIndex = 1;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(47, 386);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(27, 17);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "SD";
+            // 
+            // txtStandardDev
+            // 
+            this.txtStandardDev.Location = new System.Drawing.Point(139, 378);
+            this.txtStandardDev.Margin = new System.Windows.Forms.Padding(4);
+            this.txtStandardDev.Name = "txtStandardDev";
+            this.txtStandardDev.Size = new System.Drawing.Size(283, 22);
+            this.txtStandardDev.TabIndex = 1;
+            // 
+            // btnClose
+            // 
+            this.btnClose.ForeColor = System.Drawing.Color.Red;
+            this.btnClose.Location = new System.Drawing.Point(323, 478);
+            this.btnClose.Margin = new System.Windows.Forms.Padding(4);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(100, 28);
+            this.btnClose.TabIndex = 2;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // lblCount
+            // 
+            this.lblCount.AutoSize = true;
+            this.lblCount.Location = new System.Drawing.Point(47, 490);
+            this.lblCount.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblCount.Name = "lblCount";
+            this.lblCount.Size = new System.Drawing.Size(125, 17);
+            this.lblCount.TabIndex = 0;
+            this.lblCount.Text = "Number of Values:";
+            this.lblCount.Click += new System.EventHandler(this.lblCount_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(47, 418);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(64, 17);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "Variance";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // txtVariance
+            // 
+            this.txtVariance.Location = new System.Drawing.Point(139, 410);
+            this.txtVariance.Margin = new System.Windows.Forms.Padding(4);
+            this.txtVariance.Name = "txtVariance";
+            this.txtVariance.Size = new System.Drawing.Size(283, 22);
+            this.txtVariance.TabIndex = 1;
+            // 
+            // lbxValues
+            // 
+            this.lbxValues.FormattingEnabled = true;
+            this.lbxValues.ItemHeight = 16;
+            this.lbxValues.Location = new System.Drawing.Point(139, 106);
+            this.lbxValues.Margin = new System.Windows.Forms.Padding(4);
+            this.lbxValues.Name = "lbxValues";
+            this.lbxValues.Size = new System.Drawing.Size(132, 148);
+            this.lbxValues.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(439, 554);
+            this.Controls.Add(this.lbxValues);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtValue);
+            this.Controls.Add(this.lblCount);
+            this.Controls.Add(this.txtVariance);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtStandardDev);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtMedian);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtMode);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtMean);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "STATISTICS";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtMean;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.TextBox txtValue;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtMode;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtMedian;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtStandardDev;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Label lblCount;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtVariance;
+        private System.Windows.Forms.ListBox lbxValues;
+    }
+}
+
---
+++ TASK 3/WindowsFormsApp_ModeMedian/Form1.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp_ModeMedian
+{
+    public partial class Form1 : Form
+    {
+        List<double> values;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            values = new List<double>();
+        }
+
+        void ShowValues()
+        {
+            lbxValues.Items.Clear();
+
+            for (int i = 0; i < values.Count; i++)
+                lbxValues.Items.Add(values[i]);
+
+            lblCount.Text = values.Count.ToString();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            values.Clear();
+            lbxValues.Items.Clear();
+
+            txtValue.Text = "";
+            txtMean.Text = "";
+            txtMedian.Text = "";
+            txtStandardDev.Text = "";
+            txtVariance.Text = "";
+            txtValue.Focus();
+            txtMode.Text = "";
+
+
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            // This the value that will be added to the text box
+            double value = 0.00;
+            double sum = 0.00, sumSquares = 0.00, squareSums, mean = 0.00;
+            double median = 0.00;
+            double stdDev = 0.00;
+            double Variance = 0.00;
+
+            // Check that the user entered a value in the text box
+            if (txtValue.Text.Length == 0)
+            {
+                MessageBox.Show("You must enter a value.", "Measures of Center");
+                return;
+            }
+
+            try
+            {
+                // Get the value the user entered
+                value = double.Parse(txtValue.Text);
+                // Add it to the collection
+                values.Add(value);
+                // Show the values in the list box
+                ShowValues();
+
+                txtValue.Text = "";
+                txtValue.Focus();
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("The value you entered is invalid.",
+                                "Probability Distribution");
+            }
+
+            // Calculate the total
+            for (int i = 0; i < values.Count; i++)
+                sum += values[i];
+            squareSums = sum * sum;
+            for (int i = 0; i < values.Count; i++)
+                sumSquares += (values[i] * values[i]);
+            // Calculate the mean
+            mean = sum / values.Count;
+
+            // Get ready to evaluate the median
+            // First sort the list
+            values.Sort();
+
+            // Find out if the list is odd
+            if ((values.Count % 2) == 0)
+            {
+                double midIndex = values.Count / 2;
+                median = (values[(int)(midIndex - 0.5)] +
+                      values[(int)(midIndex + 0.5)]) / 2;
+            }
+            else
+                median = values[values.Count / 2];
+
+
+           
+
+            
+
+
+
+            double numerator = values.Count * sumSquares - squareSums;
+            double denominator = values.Count * (values.Count - 1);
+            stdDev = Math.Sqrt(numerator / denominator);
+
+            Variance = (stdDev * stdDev);
+            // Display the values
+            
+            txtMean.Text = mean.ToString("F");
+            txtMedian.Text = median.ToString("F");
+            txtStandardDev.Text = stdDev.ToString("F");
+            txtVariance.Text = Variance.ToString("F");
+            txtMode.Text = Convert.ToString(mode2());
+        }
+
+        private void lblCount_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private double mode2()
+        {
+            //Dictionary to hold the number of values for a key.
+            Dictionary<double, int> dict = new Dictionary<double, int>();
+            double mode = 0.00;
+            int max = 0;
+            foreach (double key in values)
+            {
+                if (!dict.ContainsKey(key))
+                    dict.Add(key, 1);
+                else
+                    dict[key] += 1;
+
+                if (dict[key] > max)
+                    max = dict[key];
+            }
+
+            //if the max is less than 2 return it right away to indicate an error.
+            if (max < 2)
+                return max;
+
+            SortedSet<double> keys = new SortedSet<double>(dict.Keys);
+            foreach (double key in keys)
+                if (dict[key] == max)
+                    mode = key;
+            return mode;
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
